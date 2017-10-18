cd7902d884aa20975560994a57afbbc207499d50
Morten Olsen <morten@olsen.io>
2017-10-18 10:13:02 +0200

    Fix for node install

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 git submodule init
 git submodule update
-pushd vim-config
-./install.sh
-popd
 pushd zsh-config
 ./install.sh
 popd
+export NVM_DIR="$(readlink $HOME/.zsh)nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install node
 pushd tmux-config
+./install.sh
+popd
+pushd vim-config
 ./install.sh
 popd
